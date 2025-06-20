8c08a04fb7ae1e65cc9e26c4e607d553124f8ea8
shaurya <mr.shaurya25@gmail.com>
2025-06-20 12:20:10 +0530

    Time: 2 ms (6.87%) | Memory: 44.4 MB (58.36%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -9,30 +9,31 @@
  * }
  */
 class Solution {
+
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode a = new ListNode ();
-        ListNode ans= a;
-        ListNode l = head; 
+        ListNode a = new ListNode();
+        ListNode ans = a;
+        ListNode l = head;
         int len = 0;
-        while (l!= null){
+        while (l != null) {
             len++;
-            l= l.next;
+            l = l.next;
         }
         l = head;
 
-        for (int i = 0 ; i < len/k ; i++){
-            Stack <ListNode> s = new Stack<>();
-            for (int j = 0 ; j < k ; j++ ){
-               s.push(l);
-               l = l.next;
+        for (int i = 0; i < len / k; i++) {
+            Stack<ListNode> s = new Stack<>();
+            for (int j = 0; j < k; j++) {
+                s.push(l);
+                l = l.next;
             }
-            for (int j = 0 ; j < k ; j++ ){
-               a.next = s.pop();
-               a = a.next;
+            for (int j = 0; j < k; j++) {
+                a.next = s.pop();
+                a = a.next;
             }
         }
-        a.next= l ;
+        a.next = l;
 
-         return ans.next;
+        return ans.next;
     }
 }
