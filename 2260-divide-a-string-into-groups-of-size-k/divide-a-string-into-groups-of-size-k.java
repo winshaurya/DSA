85be63228c673c2674246ddfce5612a3f449cd41
shaurya <mr.shaurya25@gmail.com>
2025-06-22 08:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (49.10%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,26 +1,14 @@
 class Solution {
 
     public String[] divideString(String s, int k, char fill) {
-        Queue<Character> q = new LinkedList<>();
-        int m =  (s.length()%k== 0) ? s.length()/k :s.length()/ k+1 ;
-        String[] st = new String[m];
-        int n = s.length();
-        int i = 0;
-        while (n-- > 0) {
-            q.offer(s.charAt(i));
-            i++;
+        StringBuilder str = new StringBuilder(s);
+        while (str.length() % k != 0) {
+            str.append(fill);
         }
-        int j = 0;
-        while (!q.isEmpty()) {
-            String str = "";
-            while (str.length() != k && !q.isEmpty()) {
-                str = str.concat(String.valueOf(q.poll()));
-            }
-            st[j++] = str;
+        String arr[] = new String[str.length() / k];
+        for (int i = 0; i < str.length() / k; i++) {
+            arr[i] = str.substring(i * k, (i + 1) * k);
         }
-        while (st[j - 1].length() != k) {
-            st[j - 1] = st[j - 1].concat(String.valueOf(fill));
-        }
-        return st;
+        return arr;
     }
 }
