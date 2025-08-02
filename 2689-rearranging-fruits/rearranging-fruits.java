777c612f22226ccd00276e48b383d0fab4e9fc28
shaurya <mr.shaurya25@gmail.com>
2025-08-02 16:48:03 +0530

    Time: 40 ms (95.77%) | Memory: 63.7 MB (84.51%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,31 +1,31 @@
 class Solution {
-
     public long minCost(int[] basket1, int[] basket2) {
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
-        int m = Integer.MAX_VALUE;
-        for (int b1 : basket1) {
-            freq.put(b1, freq.getOrDefault(b1, 0) + 1);
-            m = Math.min(m, b1);
-        }
-        for (int b2 : basket2) {
-            freq.put(b2, freq.getOrDefault(b2, 0) - 1);
-            m = Math.min(m, b2);
-        }
+                Map<Integer, Integer> freq = new HashMap<>();
 
-        List<Integer> merge = new ArrayList<>();
-        for (var entry : freq.entrySet()) {
-            int count = entry.getValue();
-            if (count % 2 != 0) return -1;
-            for (int i = 0; i < Math.abs(count) / 2; i++) {
-                merge.add(entry.getKey());
+        for (int x : basket1) freq.put(x, freq.getOrDefault(x, 0) + 1);
+        for (int x : basket2) freq.put(x, freq.getOrDefault(x, 0) - 1);
+
+        List<Integer> toSwap = new ArrayList<>();
+        for (int x : freq.keySet()) {
+            int f = freq.get(x);
+            if (f % 2 != 0) return -1; // impossible
+            for (int i = 0; i < Math.abs(f) / 2; i++) {
+                toSwap.add(x);
             }
         }
 
-        Collections.sort(merge);
-        long res = 0;
-        for (int i = 0; i < merge.size() / 2; i++) {
-            res += Math.min(2 * m, merge.get(i));
+        Collections.sort(toSwap);
+
+        int globalMin = Integer.MAX_VALUE;
+        for (int x : basket1) globalMin = Math.min(globalMin, x);
+        for (int x : basket2) globalMin = Math.min(globalMin, x);
+
+        long cost = 0;
+        int half = toSwap.size() / 2;
+        for (int i = 0; i < half; i++) {
+            cost += Math.min(toSwap.get(i), 2 * globalMin);
         }
-        return res;
+
+        return cost;
     }
 }
